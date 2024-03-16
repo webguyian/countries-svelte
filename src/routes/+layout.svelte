916e3b13081a9e7ced9c7a9ce136a7d290a621b6
Ian McPhail <webguyian@gmail.com>
2024-03-16 10:52:37 -0400

    Add Google fonts and favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,16 @@
   import "../app.css";
 </script>
 
+<svelte:head>
+  <title>Where in the world?</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,300;6..12,400;6..12,600;6..12,700&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+
 <main>
   <slot />
 </main>
